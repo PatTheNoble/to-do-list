95b9234ded49f0ed09ccc05777f58512bfbe9ce6
Patrick <patthenoble0@gmail.com>
2021-06-17 16:21:31 -0400

    Added ability to add to-dos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import ToDoItem from "./components/ToDoItem";
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
 
-function App() {
+function App(props) {
+  var [tasks, setTasks] = useState([<ToDoItem name="Default To Do"/>]);
+  var taskList = [];
+  if(tasks)
+  {
+      taskList = tasks.map(task => (
+      <ToDoItem
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+      />
+    )
+  );
+  }
+
+  function addTask(name) {
+    const newTask = { id: "ToDoItem-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>To Do List</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <div className="filters btn-group stack-exception">
+          <FilterButton />
+          <FilterButton />
+          <FilterButton />
+        </div>
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
+        {taskList}
+      </ul>
     </div>
   );
 }
